a5f037d0df58b317b91230da80be2b3a0409899e
XUNCHANG WANG <142494217+Mamekokwai@users.noreply.github.com>
2026-06-04 00:07:17 +0800

    fix:修复编辑宽度后修复内容大小

--- src/components/admin/AlbumAdmin.tsx
+++ src/components/admin/AlbumAdmin.tsx
@@ -29,7 +29,7 @@ function AdminThumbnail({ src, alt, onLoadDimensions }: { src: string, alt: stri
   }, [src])
 
   return (
-    <div className="w-[100px] h-[100px] rounded-lg overflow-hidden bg-base-300 shrink-0 my-[15px] relative">
+    <div className="aspect-square rounded-lg overflow-hidden bg-base-300 shrink-0 self-stretchrelative min-w-[100px]">
       {!loaded && !error && (
         <div className="absolute inset-0 flex items-center justify-center bg-base-200/50 backdrop-blur-[2px] z-10">
           <span className="loading loading-infinity w-8 text-primary/80"></span>
